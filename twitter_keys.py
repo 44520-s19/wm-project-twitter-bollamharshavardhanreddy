6edf5d2352ec8273e693624651f2b9deea74b629
HarshaVardhan Reddy Bollam <bollamharshavardhanreddy@gmail.com>
2019-06-19 23:27:25 -0500

    twitter project

---
+++ twitter_keys.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 22:35:55 2019
+
+@author: s534624
+"""
+consumer_key = 'ZWQCKrPiKLG7qXBcPMW80LlDO'
+consumer_secret = '1Wtn9o7E696mLib7P4S7jemCHaYUtp6wVLAUetqNaIgYdnTbk0'
+access_token = '1087703281168773121-2LHQhIZuvZ4mvi8LHJ6HUKB7osaIL2'
+access_secret = 'Aog0QMkoyEWi2zYuAEHCWNHoficQT4YHy2qqTjsHbjXpI'  
